eb8405a573ba5e0968a7a7a5b25444efa2c047c8
지운 <vonreal20@gmail.com>
2022-10-28 21:24:49 +0900

    [Common] - BaseController, deinit 구현

---
+++ PatternPracticeWithUnsplashAPI/Common/BaseViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BaseViewController.swift
+//  PatternPracticeWithUnsplashAPI
+//
+//  Created by 나지운 on 2022/10/28.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    deinit {
+        print("🙂🙂🙂🙂 deinit 🙂🙂🙂🙂")
+    }
+}
