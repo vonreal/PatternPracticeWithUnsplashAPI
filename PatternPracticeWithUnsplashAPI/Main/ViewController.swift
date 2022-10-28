b074312a23cec96db1d018e38c4e8a40936424dc
지운 <vonreal20@gmail.com>
2022-10-28 20:03:50 +0900

    [CONNECT] - 스토리보드 버튼 Outlet 연결 뷰컨

--- PatternPracticeWithUnsplashAPI/Main/ViewController.swift
+++ PatternPracticeWithUnsplashAPI/Main/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mvButton: UIButton!
+    @IBOutlet weak var mvcButton: UIButton!
+    @IBOutlet weak var mvvmButton: UIButton!
+    @IBOutlet weak var mvvmRxSwiftButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
