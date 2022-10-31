963c2fb03bc695db2adc1f5b73851a425b3cb8e2
지운 <vonreal20@gmail.com>
2022-10-31 22:42:34 +0900

    [Create] - codebase 화면 구성을 위한 BaseView 생성

---
+++ PatternPracticeWithUnsplashAPI/Common/BaseView.swift
@@ -0,0 +1,25 @@
+//
+//  BaseView.swift
+//  PatternPracticeWithUnsplashAPI
+//
+//  Created by 나지운 on 2022/10/31.
+//
+
+import UIKit
+
+class BaseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setConfigure()
+        setLayout()
+    }
+    
+    func setConfigure() { }
+    func setLayout() { }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
