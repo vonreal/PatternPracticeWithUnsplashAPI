7f0d788bc0a343a369d079de14e465356894c61c
지운 <vonreal20@gmail.com>
2022-11-01 23:01:24 +0900

    MVC, MVVM - 주석 달기

--- PatternPracticeWithUnsplashAPI/MVC/Controllers/MVCViewController.swift
+++ PatternPracticeWithUnsplashAPI/MVC/Controllers/MVCViewController.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+/* 2단계 - MVC
+    📱 코드베이스 뷰: 레이아웃, UI 객체 배치
+    🏃‍♀️ 서비스: 비즈니스 주요 로직
+    💭 뷰컨: 뷰 업데이트
+ */
+
 final class MVCViewController: BaseViewController {
 
     private let mainView = MVCView()
--- PatternPracticeWithUnsplashAPI/MVVM/View Models/MVVMViewModel.swift
+++ PatternPracticeWithUnsplashAPI/MVVM/View Models/MVVMViewModel.swift
@@ -7,6 +7,15 @@
 
 import UIKit
 
+/* 3단계 - MVVM
+    Model - View Model - View(ViewController)
+    View Model에서 Binding이 필요한데, Binding은 1. KVO, 2. FRP, 3. Delegation, 4) Boxing이 있고 Boxing 방법 사용.
+ 
+    📱 뷰(=뷰컨): 뷰만 보여줌!
+    🏃‍♀️ 뷰모델: 비즈니스 로직, 바인딩(유틸리티, Box)
+    💭 모델: 데이터 구조를 담고 있는 파일들 (RandomImage)
+ */
+
 public class MVVMViewModel {
     
     let randomImage: Box<UIImage?> = Box(nil)
